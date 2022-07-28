0d17ca2280ed9bce8208196e1d1fdefc6f2f9876
awacree <adam.acree@hatfieldmedia.com>
2022-07-28 16:27:58 -0400

    added styles

---
+++ eleventy.js
@@ -0,0 +1,15 @@
+
+module.exports = function (eleventyConfig) {
+  // Input directory: src
+  // Output directory: _site
+  eleventyConfig.addPassthroughCopy("src/css")
+  eleventyConfig.addPassthroughCopy("src/js/")
+
+
+  return {
+    dir: {
+      input: "src"
+      output: "_site"
+    }
+  }
+}
