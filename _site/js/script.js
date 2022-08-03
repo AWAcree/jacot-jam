6a6ca63307070b1006d6f001978142574c88b12c
AWAcree <mr.terrificaa@icloud.com>
2022-08-02 20:47:06 -0400

    added spotify embed and other style fixes

--- _site/js/script.js
+++ _site/js/script.js
@@ -6,42 +6,40 @@ yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener('click', function() {
-  headerEl.classList.toggle('nav-open');
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
 /////////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll('a:link');
+const allLinks = document.querySelectorAll("a:link");
 
+// allLinks.forEach(function(link){
+//   link.addEventListener('click', function(e){
+//     e.preventDefault();
+//     const href = link.getAttribute('href');
 
-allLinks.forEach(function(link){
-  link.addEventListener('click', function(e){
-    e.preventDefault();
-    const href = link.getAttribute('href');
-    
+//     // Scroll back to top
+//     if (href === "#") window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth',
+//     });
 
-    // Scroll back to top
-    if (href === "#") window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
+//     // Scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const  sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: "smooth"});
+//     }
 
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const  sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth"});
-    }
-
-    // Close mobile navigation
-    if (link.classList.contains('main-nav-link')) 
-      headerEl.classList.toggle("nav-open");
-  });
-});
+//     // Close mobile navigation
+//     if (link.classList.contains('main-nav-link'))
+//       headerEl.classList.toggle("nav-open");
+//   });
+// });
 
 /////////////////////////////////////////////////////////////
 // Sticky Navigation
@@ -49,28 +47,25 @@ allLinks.forEach(function(link){
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
-  function(entries){
-  const ent = entries[0];
+  function (entries) {
+    const ent = entries[0];
 
-  if(ent.isIntersecting === false){
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
 
-    document.body.classList.add('sticky');
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    treshold: 0,
+    rootMargin: "-80px",
   }
-   
-  if(ent.isIntersecting){
-
-    document.body.classList.remove('sticky');
-  }
-},
- {
-  // In the viewport
-  root: null,
-  treshold: 0,
-  rootMargin: '-80px',
-});
+);
 obs.observe(sectionHeroEl);
-
-
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
